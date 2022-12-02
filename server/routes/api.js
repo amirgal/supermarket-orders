a44cb5f20bad7e76e4ba5ae467b8619c21ec4d5e
amirgal <amirc538@gmail.com>
2022-12-02 19:07:51 +0200

    dd

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const pool = require('../db')
 
+
 router.get('/products', async (req,res) => {
   try {
     const products = await pool.query("SELECT * FROM products")
